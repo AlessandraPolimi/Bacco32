a25b393395c9ac58a9e6897111c09924d14c16d9
BACCHI <gif.bacco@gmail.com>
2024-05-19 16:13:11 +0200

    Initial Commit

---
+++ BACCOS/BACCOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  BACCOSApp.swift
+//  BACCOS
+//
+//  Created by Next-stel on 19/05/24.
+//
+
+import SwiftUI
+
+@main
+struct BACCOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
